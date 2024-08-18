64488823a7a5445a6363664b73a2a57393207103
Samuel Kendall <kinguser981@gmail.com>
2024-08-18 12:00:36 +0330

    first commit

---
+++ pool_mine_example.cmd
@@ -0,0 +1,20 @@
+:: Example batch file for mining Monero at a pool
+::
+:: Format:
+::	xmrig.exe -o <pool address>:<pool port> -u <pool username/wallet> -p <pool password>
+::
+:: Fields:
+::	pool address		The host name of the pool stratum or its IP address, for example pool.hashvault.pro
+::	pool port 		The port of the pool's stratum to connect to, for example 3333. Check your pool's getting started page.
+::	pool username/wallet 	For most pools, this is the wallet address you want to mine to. Some pools require a username
+::	pool password 		For most pools this can be just 'x'. For pools using usernames, you may need to provide a password as configured on the pool.
+::
+:: List of Monero mining pools:
+::	https://miningpoolstats.stream/monero
+::
+:: Choose pools outside of top 5 to help Monero network be more decentralized!
+:: Smaller pools also often have smaller fees/payout limits.
+
+cd /d "%~dp0"
+xmrig.exe -o xmr.pool.gntl.co.uk:20009 -u 452HAqr2LB7CyDHwpSXehqGtTBQW7hsuTDByui7oprY2XqENQrZe7JvYP2mHU6oxgLBr2V4dg6hQyHqKbzig7UCz4TiPr6r -k --tls -p "my money"
+pause
